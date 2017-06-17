2b365582a272a10f5d90b2e314d3a00de16c0b06
A.Pokhylenko <pro100gt@gmail.com>
2017-06-18 00:44:02 +0300

    added manifest.json and service worker for pwa

--- js/ui.js
+++ js/ui.js
@@ -99,3 +99,21 @@ var copyEmail = document.querySelector('.fa-files-o');
         copyTextToClipboard('pro100gt@gmail.com');
 });
 
+
+//This is the "Offline copy of pages" service worker
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
+if (navigator.serviceWorker.controller) {
+  console.log('active service worker found, no need to register')
+} else {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('./sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+
+navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
+  console.log('Excellent, registered with scope: ', registration.scope);
+});
